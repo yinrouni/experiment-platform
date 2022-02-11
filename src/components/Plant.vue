9ff8bce167829c25ec62e5e05842c50475e1a0fa
yinrouni <yin.ro@husky.neu.edu>
2022-02-12 01:56:53 +0800

    fix: align style in plant test

--- src/components/Plant.vue
+++ src/components/Plant.vue
@@ -10,24 +10,25 @@
       <el-col :span="12">
            <div class="type">
     <span>
+      <b>
       单选题
+      </b>
     </span>
 <br>
 <div v-for="(question, index) in questions" :key="index">
 <br>
     <div>
-      <div>
+      <div class="question-body">
         {{index +1}}.{{question.q}}
       </div>
       <div>
         <el-radio-group v-model="keys[question.id]" :disabled="submitted">
 
         <el-radio  v-for="(option, indexA) in question.a" :label="indexA" :key="option">
-          <span>{{option}}
-            <i v-if="indexA === question.k && submitted"
+           <i v-if="indexA === question.k && submitted"
             class="el-icon-circle-check right" />
             <i  v-if="indexA !== question.k && indexA === keys[question.id] && submitted" class="el-icon-circle-close wrong" />
-
+          <span style="white-space: pre">{{option}}
           </span>
         </el-radio>
         </el-radio-group>
@@ -133,26 +134,26 @@ export default {
         {
           id: 1,
           q: '关于即刻种植,下列说法有误的是 ( )',
-          a: ['A．美学区即刻种植充分利用原有牙根根尖及根周壁骨量以获得好的初期稳定',
-            'B．尽可能选用长植体',
-            'C．间隙植入骨替代材料减少唇侧塌陷 ',
-            'D．采取翻瓣术式，有利于提高修复满意度', 'E．利于牙龈塑形,软组织美学'],
+          a: [
+            'A．尽可能选用长植体            ',
+            'B．间隙植入骨替代材料减少唇侧塌陷 ',
+            'C．利于牙龈塑形,软组织美学', 'D．采取翻瓣术式，有利于提高修复满意度',],
           k: 3
         },
         {
           id: 2,
           q: '即刻种植中,种植体植入时的临床状态是（ ）',
-          a: ['A．拔牙位点没有骨和软组织愈合',
+          a: ['A．拔牙位点没有骨和软组织愈合 ',
             'B．拔牙位点软组织愈合',
-            'C．拔牙位点骨愈合',
-            'D．拔牙位点软组织愈合,骨未愈合',
-            'E．拔牙位点完全愈合'],
+            'C．拔牙位点软组织愈合,骨未愈合',
+            'D．拔牙位点骨愈合'
+           ],
           k: 0
         },
         {
           id: 3,
           q: '种植体植入的外科手术操作,以下哪项错误（ ）',
-          a: ['A．提拉式备洞方法',
+          a: ['A．提拉式备洞方法             ',
             'B．先锋钻进行种植床制备转速800钻/分',
             'C．冷却生理盐水温度为4℃',
             'D．备洞的宽度大于种植体的宽度' ],
@@ -191,5 +192,8 @@ export default {
   }
   video{
     width: 100%;
+  }
+  .question-body{
+    padding-bottom: 7px;
   }
 </style>
