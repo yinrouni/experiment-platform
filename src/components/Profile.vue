8c0dc8fb7fa2a04c20dec0abe7de489941523bb1
yinrouni <yin.ro@husky.neu.edu>
2022-02-16 00:50:16 +0800

    style: font size

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,40 +4,43 @@
 <el-row >
   <el-col :span="12">
      <el-descriptions class="margin-top" title="病例资料" :column="1" border>
+       <div slot="title">
+         <h3> 病例资料</h3>
+       </div>
     <el-descriptions-item>
       <template slot="label">
         <i class="el-icon-user"></i>
-        姓名
+        <span class="text">姓名</span>
       </template>
-      聂非
+     <span class="text"> 聂非</span>
     </el-descriptions-item>
     <el-descriptions-item>
       <template slot="label">
         <i class="el-icon-mobile-phone"></i>
-        性别
+       <span class="text"> 性别</span>
       </template>
-      男
+      <span class="text">男</span>
     </el-descriptions-item>
     <el-descriptions-item>
       <template slot="label">
         <i class="el-icon-location-outline"></i>
-        年龄
+        <span class="text">年龄</span>
       </template>
-      28
+     <span class="text"> 28</span>
     </el-descriptions-item>
     <el-descriptions-item>
       <template slot="label">
         <i class="el-icon-tickets"></i>
-        职业
+        <span class="text">职业</span>
       </template>
-      <el-tag size="small">海洋摄影师</el-tag>
+      <el-tag size="medium">海洋摄影师</el-tag>
     </el-descriptions-item>
     <el-descriptions-item>
       <template slot="label">
         <i class="el-icon-office-building"></i>
-        联系地址
+       <span class="text"> 联系地址</span>
       </template>
-      四川省成都市武侯区一环路南段
+     <span class="text"> 四川省成都市武侯区一环路南段</span>
     </el-descriptions-item>
   </el-descriptions>
 
@@ -73,4 +76,9 @@ export default {
   img{
     width: 350px;
   }
+  .el-descriptions__title{
+    font-size: 18px;
+  }
+
+
 </style>
