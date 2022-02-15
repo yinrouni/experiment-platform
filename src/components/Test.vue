2d65ae09882fa53a8fdad5a04f8c8bf59126d7de
yinrouni <yin.ro@husky.neu.edu>
2022-02-16 00:26:26 +0800

    style: menu

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,48 +1,50 @@
 <template>
   <el-container style="border: 1px solid #eee">
   <el-aside width="200px">
-     <el-menu :default-active="index" @select="select">
+    <div class="menu">
+     <el-menu :default-active="index" @select="select" :background-color="'#fafafa'" :active-text-color="'#409EFF'">
         <el-menu-item index="1">
         <i class="el-icon-message"></i>
-        <span slot="title">病例资料</span>
+        <span slot="title" class="tab">病例资料</span>
      </el-menu-item>
      <el-menu-item index="2">
         <i class="el-icon-edit"></i>
-        <span slot="title">病史采集</span>
+        <span slot="title" class="tab">病史采集</span>
      </el-menu-item>
      <el-menu-item index="3">
         <i class="el-icon-tickets"></i>
-        <span slot="title">方案商议</span>
+        <span slot="title" class="tab">方案商议</span>
      </el-menu-item>
         <el-menu-item index="4">
         <i class="el-icon-document-checked"></i>
-        <span slot="title">知情同意</span>
+        <span slot="title" class="tab">知情同意</span>
      </el-menu-item>
         <el-menu-item index="5">
         <i class="el-icon-suitcase"></i>
-        <span slot="title">术前准备</span>
+        <span slot="title" class="tab">术前准备</span>
      </el-menu-item>
 
         <el-menu-item index="6">
         <i class="el-icon-monitor"></i>
-        <span slot="title">牙拔除术</span>
+        <span slot="title" class="tab">牙拔除术</span>
      </el-menu-item>
 
         <el-menu-item index="7">
         <i class="el-icon-coordinate"></i>
-        <span slot="title" >即刻种植</span>
+        <span slot="title" class="tab" >即刻种植</span>
      </el-menu-item>
 
         <el-menu-item index="8">
         <i class="el-icon-bell"></i>
-        <span slot="title">术后医嘱</span>
+        <span slot="title" class="tab">术后医嘱</span>
      </el-menu-item>
 
       <el-menu-item index="9">
         <i class="el-icon-circle-check"></i>
-        <span slot="title">评分</span>
+        <span slot="title" class="tab">评分</span>
      </el-menu-item>
     </el-menu>
+    </div>
   </el-aside>
   <el-container>
     <el-main>
@@ -122,4 +124,20 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
   }
+  .tab{
+    font-size: 16px;
+    font-weight: 450;
+  }
+
+  .el-menu-item:hover {
+	background: #ecf5ff !important;
+}
+  .el-menu-item.is-active{
+      border-right: #409EFF solid 4px;
+      	background: #ecf5ff !important;
+  }
+
+.el-aside{
+  background: #fafafa
+}
 </style>
