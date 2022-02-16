fb5a13b635a30be2626c515c1f165e0ca8050884
yinrouni <yin.ro@husky.neu.edu>
2022-02-17 01:12:58 +0800

    fix: bundle size

---
+++ src/plugins/element.js
@@ -0,0 +1,87 @@
+import Vue from 'vue'
+import {
+  Dialog,
+  Menu,
+  Submenu,
+  MenuItem,
+  MenuItemGroup,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  Tag,
+  Icon,
+  Row,
+  Col,
+  Progress,
+  Spinner,
+  Badge,
+  Card,
+  Steps,
+  Step,
+  Collapse,
+  CollapseItem,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Divider, 
+  Descriptions, 
+  DescriptionsItem,
+  Avatar, 
+  SkeletonItem
+
+} from 'element-ui'
+
+Vue.use(Dialog)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(RadioButton)
+Vue.use(Checkbox)
+Vue.use(CheckboxButton)
+Vue.use(CheckboxGroup)
+Vue.use(Option)
+Vue.use(OptionGroup)
+Vue.use(Button)
+Vue.use(ButtonGroup)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Tag)
+Vue.use(Icon)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Progress)
+Vue.use(Spinner)
+Vue.use(Badge)
+Vue.use(Card)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Divider)
+Vue.use(Descriptions)
+Vue.use(DescriptionsItem)
+Vue.use(Avatar)
+Vue.use(SkeletonItem)
