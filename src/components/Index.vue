b47eaca14d456370d0ed49fc5707755db4f4075a
yinrouni <yin.ro@husky.neu.edu>
2022-02-19 21:58:14 +0800

    fix: index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -8,44 +8,6 @@
 import Intro from './Intro'
 export default {
   components: {Intro}
-  // data () {
-  //   return {
-  //     // activeIndex: '1',
-  //     // arrDom: [],
-  //     // headerFixed: false
-  //   }
-  // },
-  // mounted () {
-  //   this.arrDom = document.getElementsByClassName('item-content')
-  //   window.addEventListener('scroll', this.handleScroll)
-  // },
-
-  // destroyed () {
-  //   window.removeEventListener('scroll', this.handleScroll)
-  // },
-  // methods: {
-  //   handleScroll () {
-  //     let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
-  //     for (let i = 0; i < this.arrDom.length; i++) {
-  //       if (this.arrDom[this.arrDom.length - 1].offsetTop - scrollTop > 760) {
-  //         if (this.arrDom[i].offsetTop - scrollTop <= 760 && this.arrDom[i + 1].offsetTop - scrollTop > 760) {
-  //           this.activeIndex = (i + 1) + ''
-  //         }
-  //       } else {
-  //         this.activeIndex = this.arrDom.length + ''
-  //       }
-  //     }
-  //   },
-  //   handleSelect (key, keyPath) {
-  //     this.activeIndex = key
-  //     let pageId = document.querySelector('#page' + key)
-  //     let nav = document.getElementById('nav-container')
-  //     window.scrollTo({
-  //       'top': pageId.offsetTop - nav.offsetHeight,
-  //       'behavior': 'smooth'
-  //     })
-  // }
-  // }
 }
 </script>
 
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
-    <img src="https://imgs-videos.oss-cn-beijing.aliyuncs.com/IMG_5662.png" style="width: 100%; height: 100%"/>
+    <img src="https://imgs-videos.oss-cn-beijing.aliyuncs.com/IMG_6006.JPG" style="width: 100%; height: 100%"/>
     <div class="name">
       <h1 style="font-size: 3em">
-        前牙即刻种植修复虚拟仿真实验
+        前牙即刻种植修复
       </h1>
-
-       <el-button type="primary" plain round @click="enter">马上体验</el-button>
-<br>
-
+       <h1 style="font-size: 3em">
+    虚拟仿真实验
+      </h1>
+      <br>
       <h2> 华理种植小分队</h2>
+      <br>
+      <br>
+      <br>
+       <el-button type="primary" plain round @click="enter">马上体验</el-button>
     </div>
   </div>
 
@@ -32,8 +36,9 @@ export default {
   .name{
     color: white;
     position: absolute;
-    top: 80%;
-    left: 50%;
+    top: 50%;
+    left: 75%;
     transform: translate(-50%, -50%);
+    width: 50%;
   }
 </style>
